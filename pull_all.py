72fc231537955f8c603fb0e056c262969ebfaf73
Theodor Kulczycki <tkulczy2@gmail.com>
2015-09-08 17:59:49 -0500

    completed pull_all for year 2014

--- pull_all.py
+++ pull_all.py
@@ -7,7 +7,30 @@ from ecmwfapi import ECMWFDataServer
 # To run this example, you need an API key
 # available from https://api.ecmwf.int/v1/key/
 
-name = 't2m_5yr'
+name = '2014'
+
+fields = ['param','step','time','type']
+parameters = ['2t','mx2t','mn2t','tp']
+
+params = {'2t':"167.128",
+          'mx2t':"201.128",
+          'mn2t':"202.128",
+          'tp':"228.128"}
+
+steps = {'2t':"0",
+         'mx2t':"03/06/09/12",
+         'mn2t':"03/06/09/12",
+         'tp':"03/06/09/12"}
+
+times = {'2t':"00/06/12/18",
+         'mx2t':"00/12",
+         'mn2t':"00/12",
+         'tp':"00/12"}
+
+types = {'2t':"an",
+         'mx2t':"fc",
+         'mn2t':"fc",
+         'tp':"fc"}
 
 outputDir = os.path.expandvars('$HOME/ERA_Interim/'+name+'/')
 
@@ -16,14 +39,7 @@ try:
 except OSError:
     if not os.path.isdir(outputDir):
         raise
-'param','step','time','type'
-codes  = {'2t':"167.128",
-          'mx2t24':"51.128",
-          'mv2t24':"52.128",
-          'mx2t6':"121.128",
-          'mn2t6':"122.128",
-          'tp':"228.128"}
-steps = {}
+
 server = ECMWFDataServer()
 
 startYear = 2014
@@ -33,17 +49,20 @@ if endYear==2015:
     endDate = str(endYear) + '-06-30'
 else:
     endDate = str(endYear) + '-12-31'
-server.retrieve({
-    'stream'    : "oper",
-    'levtype'   : "sfc",
-    'param'     : "167.128",
-    'dataset'   : "interim",
-    'step'      : "0",
-    'grid'      : "0.25/0.25",
-    'time'      : "00",
-    'date'      : "{}/to/{}".format(startDate, endDate),
-    'type'      : "an",
-    'class'     : "ei",
-    'format'    : "netcdf",
-    'target'    : "{}erai_{}_{}to{}.nc".format(outputDir, startDate, endDate)
-})
+
+
+for p in parameters:
+    server.retrieve({
+        'stream'    : "oper",
+        'levtype'   : "sfc",
+        'param'     : params[p],
+        'dataset'   : "interim",
+        'step'      : steps[p],
+        'grid'      : "0.25/0.25",
+        'time'      : times[p],
+        'date'      : "{}/to/{}".format(startDate, endDate),
+        'type'      : types[p],
+        'class'     : "ei",
+        'format'    : "netcdf",
+        'target'    : "{}erai_{}_{}to{}.nc".format(outputDir, p, startDate, endDate)
+    })
