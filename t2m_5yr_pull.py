a70e182b505a30242362482cc205647f70390f73
Theodor Kulczycki <theodorkulczycki@sscss-MacBook-Pro.local>
2015-09-08 12:41:55 -0500

    output directory specification

--- t2m_5yr_pull.py
+++ t2m_5yr_pull.py
@@ -1,30 +1,42 @@
 #!/usr/bin/env python
 __author__ = 'theodorkulczycki'
 
+import os
 from ecmwfapi import ECMWFDataServer
 
 # To run this example, you need an API key
 # available from https://api.ecmwf.int/v1/key/
 
+name = 't2m_5yr'
+
+outputDir = os.path.expandvars('$HOME/ERA_Interim/'+name+'/')
+
+try:
+    os.makedirs(outputDir)
+except OSError:
+    if not os.path.isdir(outputDir):
+        raise
+
 server = ECMWFDataServer()
 
-for year in range(1979,2016):
-    startDate = str(year) + '-01-01'
-    if year==2015:
-        endDate = str(year) + '-06-30'
-    else:
-        endDate = str(year) + '-12-31'
-    server.retrieve({
-        'stream'    : "oper",
-        'levtype'   : "sfc",
-        'param'     : "165.128",
-        'dataset'   : "interim",
-        'step'      : "0",
-        'grid'      : "0.75/0.75",
-        'time'      : "00/06/12/18",
-        'date'      : "%s/to/%s"%(startDate,endDate),
-        'type'      : "an",
-        'class'     : "ei",
-        'format'    : "netcdf",
-        'target'    : "era-i_%sto%s_00061218.nc"%(startDate,endDate)
-    })
+startYear = 1979
+endYear = 1984
+startDate = str(startYear) + '-01-01'
+if year==2015:
+    endDate = str(endYear) + '-06-30'
+else:
+    endDate = str(endYear) + '-12-31'
+server.retrieve({
+    'stream'    : "oper",
+    'levtype'   : "sfc",
+    'param'     : "167.128",
+    'dataset'   : "interim",
+    'step'      : "0",
+    'grid'      : "0.25/0.25",
+    'time'      : "00",
+    'date'      : "{}/to/{}".format(startDate, endDate),
+    'type'      : "an",
+    'class'     : "ei",
+    'format'    : "netcdf",
+    'target'    : "{}era-i_{}to{}_00061218.nc"%(outputDir, startDate, endDate)
+})
