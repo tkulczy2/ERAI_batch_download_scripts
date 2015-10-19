76a32598f207eef9775e1b9c549bf8143e5f167c
Theodor Kulczycki <tkulczy2@gmail.com>
2015-10-19 16:46:34 -0500

    fixed type in getExtrasForSol

--- getExtrasForSol.py
+++ getExtrasForSol.py
@@ -3,5 +3,5 @@ __author__ = 'theodorkulczycki'
 
 import getParameterYear
 for p in ['20','57','58','169','182','189','210','212']:
-    for y in range(1979,2015):
+    for y in [str(x) for x in range(1979,2015)]:
         getParameterYear.get(p, y)
