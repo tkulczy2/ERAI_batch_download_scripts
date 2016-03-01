d372332e100ccda2aebbf116c842d4db72eb582c
Theodor Kulczycki <tkulczycki@shackleton.Berkeley.EDU>
2016-03-01 11:47:26 -0800

    adding template projection script

---
+++ formatting_scripts_daily_polynomials/PROJECTION_SCRIPTS_v2/testing_latitude_USA.m
@@ -0,0 +1,9 @@
+cd /mnt/norgay/Datasets/Climate/ERA_Interim/formatting_scripts_daily_polynomials/PROJECTION_SCRIPTS_v2
+%USA_projection_2000_poly %dropping data at -90 latitude
+
+cd /mnt/norgay/Datasets/Climate/ERA_Interim/formatting_scripts_daily_polynomials/PROJECTION_SCRIPTS_v2
+USA_projection_2000_poly_drop90 %dropping data at +90 latitude
+
+cd /mnt/norgay/Datasets/Climate/ERA_Interim/formatting_scripts_daily_polynomials/PROJECTION_SCRIPTS_v2
+USA_projection_2000_poly_drop0 %dropping data at 0 latitude
+
